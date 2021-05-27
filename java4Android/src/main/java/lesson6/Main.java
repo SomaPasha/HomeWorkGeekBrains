b34b204bb2d4f743f691d4aef02beff676a232a1
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-05-28 00:51:31 +0500

    Урок шестой (c домашкой с презентацией)

--- java4Android/src/main/java/lesson6/Main.java
+++ java4Android/src/main/java/lesson6/Main.java
@@ -1,5 +1,22 @@
 package lesson6;
 
+import java.util.ArrayList;
+
+class Zoo {
+    private ArrayList<Animal> arrAnimal = new ArrayList<Animal>();
+
+    public void addAnimal(Animal... animal) {
+        for (Animal arr : animal) {
+            arrAnimal.add(arr);
+        }
+
+    }
+
+    public ArrayList<Animal> getAnimal() {
+        return arrAnimal;
+    }
+}
+
 class Animal {
     private String name;
     private static int quantityAnimal = 0;
@@ -26,12 +43,29 @@ class Animal {
         this.name = name;
     }
 
-    public void run(int distanse) {
-        System.out.println(this.name + " пробежал(а) " + distanse + " м.");
+    public void run(int distanse, int limit) {
+        if (distanse <= limit) {
+            System.out.println(this.name + " run: " + (distanse <= limit));
+        } else {
+            System.out.println(this.name + " run: " + (distanse <= limit));
+        }
+        //   System.out.println(this.name + " пробежал(а) " + distanse + " м. true");
     }
 
-    public void swim(int distanse) {
-        System.out.println(this.name + " проплыл(а) " + distanse + " м.");
+    public void swim(int distanse, int limit) {
+        if (distanse <= limit) {
+            System.out.println(this.name + " swim: " + (distanse <= limit));
+        } else {
+            System.out.println(this.name + " swim: " + (distanse <= limit));
+        }
+    }
+
+    public void jump(float height, float limit) {
+        if (height <= limit) {
+            System.out.println(this.name + " jump: " + (height <= limit));
+        } else {
+            System.out.println(this.name + " jump: " + (height <= limit));
+        }
     }
 }
 
@@ -45,6 +79,7 @@ class Cat extends Animal {
 
     }
 
+
     public Cat(String name) {
         super(name + " кот");
         quantityCat++;
@@ -55,17 +90,8 @@ class Cat extends Animal {
     }
 
     @Override
-    public void run(int distanse) {
-        if (distanse <= 200) {
-            super.run(distanse);
-        } else {
-            System.out.println(getName() + " может пробежать только 200 м. Осталось " + (distanse - 200));
-        }
-    }
-
-    @Override
-    public void swim(int distanse) {
-        System.out.println(getName() + " не умеет плавать");
+    public void swim(int distanse, int limit) {
+        System.out.println(getName() + " swim false");
     }
 }
 
@@ -87,55 +113,66 @@ class Dog extends Animal {
         return quantityDog;
     }
 
-    @Override
-    public void run(int distanse) {
-        if (distanse <= 500) {
-            super.run(distanse);
-        } else {
-            System.out.println(getName() + " может пробежать только 500 м. Осталось  " + (distanse - 500));
-        }
-    }
-
-    @Override
-    public void swim(int distanse) {
-        if (distanse <= 10) {
-            super.swim(distanse);
-        } else {
-            System.out.println(getName() + " может проплыть только 10 м. Осталось " + (distanse - 10));
-        }
-    }
 }
 
 public class Main {
+    private static int quantityCat = 0;
+    private static int quantityAnimal = 0;
+    private static int quantityDog = 0;
 
     public static void main(String[] args) {
+
+        Zoo zoo = new Zoo();
         Animal animal = new Animal("Зверь");
         Animal animal1 = new Animal();
         Cat cat = new Cat();
         Cat cat1 = new Cat("Багира");
         Dog dog = new Dog();
         Dog dog1 = new Dog("Шарик");
-        animal.run(1000);
-        animal1.run(1000);
-        cat.run(1000);
-        cat1.run(1000);
-        dog.run(1000);
-        dog1.run(1000);
+        animal.run(1000, 100000);
+        animal1.run(1000, 100000);
+        cat.run(100, 200);
+        cat1.run(100, 200);
+        dog.run(100, 400);
+        dog1.run(700, 600);
 
-        animal.swim(1);
-        animal1.swim(1);
-        cat.swim(1);
-        cat1.swim(1);
-        dog.swim(1);
-        dog1.swim(1);
+        animal.swim(1, 10000);
+        animal1.swim(1, 10000);
+        cat.swim(1, 0);
+        cat1.swim(1, 0);
+        dog.swim(1, 10);
+        dog1.swim(1, 10);
+
+        animal.jump(1, 10);
+        animal1.jump(1, 10);
+        cat.jump(1, 2);
+        cat1.jump(1, 2);
+        dog.jump((float) 0.3, 0.5F);
+        dog1.jump(10, 0.5F);
+
+        zoo.addAnimal(animal, animal1, cat, cat1, dog, dog1);
+        zoo.getAnimal().forEach(ani -> {
+            if (ani instanceof Cat) {
+                quantityCat++;
+            }
+            if (ani instanceof Dog) {
+                quantityDog++;
+            }
+            quantityAnimal++;
+        });
+
         countQuantityAnimalAndByCategory();
-
     }
 
     public static void countQuantityAnimalAndByCategory() {
+        //Реализация подсчёта через static
         System.out.println("Итого создано: \n" + Animal.getQuantityAnimal() + " животных.\n"
                 + Cat.getQuantityCat() + " котов.\n"
                 + Dog.getQuantityDog() + " собак.");
+        //Реализация подсчёта через Класс Зоопарк
+        System.out.println("Итого создано: \n" + quantityAnimal + " животных.\n"
+                + quantityCat + " котов.\n"
+                + quantityDog + " собак.");
     }
 
 
